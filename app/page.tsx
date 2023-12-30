afc37595492e9713c8d8d8c80db4c45a647423bf
jojokimys <jojokimys@gmail.com>
2023-12-31 04:03:23 +0900

    feat: added react-toastify

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image'
+import { ToastContainer, toast } from 'react-toastify';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+      <ToastContainer />
     </main>
   )
 }
